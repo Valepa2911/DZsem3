6cde9302734c26490d2321e1ce6e130f9d4940f9
Valepa2911 <megp17102017@mail.ru>
2022-12-06 20:28:10 +0400

    сохран4.

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int numA;
+bool res = int.TryParse(number, out numA);
+if (res == true)
+//int number = Convert.ToInt32(Console.ReadLine());
+   for (int i=1; i<=numA; i++)
+       Console.WriteLine(i*i*i);
+else
+Console.WriteLine("Это не число");       
