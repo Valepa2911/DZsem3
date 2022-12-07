558b9ea78ade452c5382e9a9d33c58a451ec24f0
Valepa2911 <megp17102017@mail.ru>
2022-12-07 18:00:34 +0400

    сохран6.

--- Zadacha21/Program.cs
+++ Zadacha21/Program.cs
@@ -1,7 +1,8 @@
 ﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-double = ab;
-double point = new double[6];
-for (int=0; i<point.Length; i++)
+double ab = 0;
+double[] point = new double[6];
+for (int i=0; i<point.Length; i++)
 point[i] = Convert.ToDouble(Console.ReadLine());
-ab = Math.Sqrt((point[2]-point[1])*(point[2]-point[1]))
+ab = Math.Sqrt((point[3]-point[0])*(point[3]-point[0]) + (point[4]-point[1])*(point[4]-point[1]) + (point[5]-point[2])*(point[5]-point[2]));
+Console.WriteLine(ab);
